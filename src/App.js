d6a2995c7827eab24edeea87d2653f657c0a148f
An-hyeonyoung <79887939+An-hyeonyoung@users.noreply.github.com>
2023-09-18 11:55:30 +0900

    Add-메인페이지 컨텐츠 추가

--- src/App.js
+++ src/App.js
@@ -9,12 +9,12 @@ function App() {
   return (
     <div className="App">
       <MainNav />
-      <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="/members" element={<div>Members임</div>} />
-        <Route path="/blog" element={<div>Blog임</div>} />
-        <Route path="*" element={<div>없는페이지임</div>} />
-      </Routes>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/members" element={<div>Members임</div>} />
+          <Route path="/blog" element={<div>Blog임</div>} />
+          <Route path="*" element={<div>없는페이지임</div>} />
+        </Routes>
     </div>
   );
 }
--- src/MainNav.js
+++ src/MainNav.js
@@ -2,19 +2,18 @@
 
 import { Container, Nav, Navbar } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
-import logoNav from './img/logo-nav.png';
 
 function MainNav() {
     // 페이지 이동 함수
     let navigate = useNavigate();
 
     return (
-        <Navbar expand="lg">
+        <Navbar className="bg-white" expand="lg" bg="light" data-bs-theme="light" fixed="top">
             <Container>
                 <Navbar.Brand onClick={() => { navigate('/') }}>
                     <img
                         alt=""
-                        src={logoNav}
+                        src='img/logo-nav.png'
                         width="100"
                         height="30"
                         className="d-inline-block align-top"
